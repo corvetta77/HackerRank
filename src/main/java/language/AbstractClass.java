4f383dcb08583b6a8919b9d5a0a5a2973ec5909c
corvetta77 <szczepan.kurnyta@gmail.com>
2023-04-24 08:52:11 +0200

    Adding latest exercises

---
+++ src/main/java/language/AbstractClass.java
@@ -0,0 +1,53 @@
+package language;
+
+import java.util.*;
+
+abstract class Book {
+    String title;
+    String author;
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    abstract void display();
+}
+
+class MyBook extends Book {
+    private final int price;
+
+    public MyBook(String title, String author, int price) {
+        super(title, author);
+        this.price = price;
+    }
+
+    @Override
+    void display() {
+        System.out.printf("Title: %s\n", title);
+        System.out.printf("Author: %s\n", author);
+        System.out.printf("Price: %d\n", price);
+    }
+}
+/**
+ *   Method Name: display
+ *
+ *   Print the title, author, and price in the specified format.
+ **/
+// Write your method here
+
+// End class
+
+public class AbstractClass {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
+        scanner.close();
+
+        Book book = new MyBook(title, author, price);
+        book.display();
+    }
+}
---
+++ src/main/java/language/Solution.java
@@ -0,0 +1,57 @@
+package language;
+
+import java.util.*;
+
+public class Solution {
+    // Write your code here.
+    private List<Character> queue = new LinkedList<>();
+    private List<Character> stack = new LinkedList<>();
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        scan.close();
+
+        // Convert input String to an array of characters:
+        char[] s = input.toCharArray();
+
+        // Create a Solution object:
+        Solution p = new Solution();
+
+        // Enqueue/Push all chars to their respective data structures:
+        for (char c : s) {
+            p.pushCharacter(c);
+            p.enqueueCharacter(c);
+        }
+
+        // Pop/Dequeue the chars at the head of both data structures and compare them:
+        boolean isPalindrome = true;
+        for (int i = 0; i < s.length/2; i++) {
+            if (p.popCharacter() != p.dequeueCharacter()) {
+                isPalindrome = false;                
+                break;
+            }
+        }
+
+        //Finally, print whether string s is palindrome or not.
+        System.out.println( "The word, " + input + ", is " 
+                           + ( (!isPalindrome) ? "not a palindrome." : "a palindrome." ) );
+    }
+
+    private Character dequeueCharacter() {
+        return queue.get(0);
+    }
+
+    private void enqueueCharacter(char c) {
+        queue.add( c);
+    }
+
+    private Character popCharacter() {
+        return stack.get(stack.size()-1);
+    }
+
+    private void pushCharacter(char c) {
+        stack.add(c);
+    }
+}
